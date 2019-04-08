b78ed651e8b1918ce55ec89fdad15306d998985f
Dmitriy Kovalev <kodimweb@gmail.com>
2019-04-08 15:38:28 +0300

    update db

--- yt.js
+++ yt.js
@@ -10,7 +10,8 @@ console.log('/___/___/ \\______/ \\______//___/___//_______//______/ \\______/')
 const EMAIL  = 'kacevnik@gmail.com';
 const PASS   = 'G9564665g';
 //const SEARCH = 'РОБЛОКС';
-const SEARCH = 'Roblox';
+//const SEARCH = 'Roblox';
+const SEARCH = 'Егор';
 const URL    = 'https://youtube.com';
 
 let sec = 0;
